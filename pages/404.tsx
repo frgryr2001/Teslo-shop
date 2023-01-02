0b7cd04f1e78ab990e20a1c579a29d71a8f8ea13
nhân <nhanle.lx@gmail.com>
2023-01-02 12:37:58 +0700

    homepage

---
+++ pages/404.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ShopLayout } from "../components/layouts/ShopLayout";
+import { Box, Typography } from "@mui/material";
+
+const Custom404 = () => {
+  return (
+    <ShopLayout
+      title={"Page not found"}
+      pageDescription={"Nothing to show here "}
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems={"center"}
+        height="calc(100vh - 200px)"
+        sx={{ flexDirection: { xs: "column", sm: "row" } }}
+      >
+        <Typography
+          variant="h1"
+          component={"h1"}
+          fontSize={80}
+          fontWeight={200}
+        >
+          404 |
+        </Typography>
+        <Typography marginLeft={2}>Nothing to show here</Typography>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default Custom404;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 export default function Home() {
   return (
-    <ShopLayout
-      title={"Teslo-Shop"}
-      pageDescription={
-        "Clothing shop is is a fashion brand that offers the latest models today"
-      }
-    >
-      <Typography variant="h1" component="h1">
-        Store
-      </Typography>
-      <Typography variant="h2" sx={{ mb: 1 }}>
-        All Products
-      </Typography>
-    </ShopLayout>
+    <>
+      <ShopLayout
+        title={"Teslo-Shop"}
+        pageDescription={
+          "Clothing shop is is a fashion brand that offers the latest models today"
+        }
+      >
+        <Typography variant="h1" component="h1">
+          Store
+        </Typography>
+        <Typography variant="h2" sx={{ mb: 1 }}>
+          All Products
+        </Typography>
+
+        <Grid container spacing={4}>
+          {initialData.products.map((product) => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component={"img"}
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  ></CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </ShopLayout>
+    </>
   );
 }
